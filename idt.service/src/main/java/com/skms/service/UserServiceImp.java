8a338c7b850b53200d218dda878bb872bbcb53ff
chenyaohua1990 <chenyaohua1990@foxmail.com>
2017-10-16 04:41:05 +0800

    hessian项目搭建第一次提交

---
+++ idt.service/src/main/java/com/skms/service/UserServiceImp.java
@@ -0,0 +1,13 @@
+package com.skms.service;
+
+import com.skms.api.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+@Service("userServiceImp")
+public class UserServiceImp implements UserService {
+
+    public String login(Map<String, String> map) {
+        return null;
+    }
+}
